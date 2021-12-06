e1b75e440cd4fa6d82f72c68a0d938b261c01298
Dhruv Desai <dhruvshah28@gmail.com>
2021-12-06 15:25:30 +0530

    Comments Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { React, useState, useEffect } from 'react'
-import img from './image.png';
+import speedNumbers from './image.png';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); // Initialising Count value
   // let countArc = count + 50;
-  var arclen = Math.PI * 50 * (360 - count) / 180.0;
+  var arclength = Math.PI * 50 * (360 - count) / 180.0; //Calculating Arc Length
   useEffect(() => {
     const interval = setInterval(() => {
       let num = count + 1;
@@ -19,33 +19,29 @@ function App() {
   });
   return (
     <>
-
       <div className="App">
         <div className="mainCircle">
-          <svg className="svg" width="455" height="455" viewBox="0 0 120 120">
-          {/* <!-- Circle element of radius 50 units. --> */}
-          {/* <!-- (Rotated 90° CCW to put start point is at top.) --> */}
-          <circle id="c" cx="60" cy="60"
+          <svg className="arcCurve" width="455" height="455" viewBox="0 0 120 120">
+          {/* (Rotated 90° CCW to put start point is at base.) */}
+          <circle cx="60" cy="60"
             r="42" fill="none" stroke="#EE9737"
             stroke-width="2.5"
-            stroke-dashoffset={arclen}
+            stroke-dashoffset={arclength}
             stroke-dasharray="314.16"
             transform="rotate(-224 60 60)"
             stroke-linecap="round"
           />
         </svg>
-          <img src={img} className="img" />
-          <div className="circle">
-            <div className="text">
+          <img src={speedNumbers} />
+          <div className="innerCircle">
+            <div className="innerText">
               {count}
             </div>
-            <div className="kmph">
+            <div className="kmhText">
               kmh
             </div>
           </div>
         </div>
-
-        
       </div>
     </>
   );
